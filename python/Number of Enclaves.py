7d9534a32a01fdf0996478d04b873618efbbca27
sreekanth <132795541+sreekanth-ai@users.noreply.github.com>
2023-09-29 04:42:07 +0530

    Create Number of Enclaves.py

---
+++ python/Number of Enclaves.py
@@ -0,0 +1,35 @@
+import sys
+from typing import List
+sys.setrecursionlimit(10**8)
+
+#recursive function to perform depth-first search
+def dfs(i, j, grid):
+    if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0:
+        return
+    
+    #mark the current cell as visited (0)
+    grid[i][j] = 0
+    dfs(i + 1, j, grid)
+    dfs(i, j + 1, grid)
+    dfs(i, j - 1, grid)
+    dfs(i - 1, j, grid)
+
+class Solution:
+    #function to calculate the number of enclaves
+    def numberOfEnclaves(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        #perform DFS on boundary cells
+        for i in range(n):
+            for j in range(m):
+                if i == 0 or j == 0 or i == n - 1 or j == m - 1 and grid[i][j] == 1:
+                    dfs(i, j, grid)
+        
+        #count the remaining enclaves
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                count += grid[i][j]
+
+        return count
